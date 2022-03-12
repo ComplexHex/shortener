86c95ff8d40aaa3fc8baaa108d0a42897e81fb12
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-03-12 09:45:48 +0300

    v3

--- Shortener.java
+++ Shortener.java
@@ -1,14 +1,30 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+import java.util.HashMap;
+
 public class Shortener {
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
 
-
-    public Long getId(String string){
-        return 0L;
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id){
-        return "";
+    public synchronized Long getId(String string) {
+        if (storageStrategy.containsValue(string)) {
+            return storageStrategy.getKey(string);
+        } else {
+            lastId = lastId++;
+            storageStrategy.put(lastId, string);
+        }
+
+        return lastId;
+    }
+
+    public synchronized String getString(Long id) {
+        return storageStrategy.getValue(id);
     }
 
 
