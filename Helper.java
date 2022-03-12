47505f673beafcc9f169912f38d8d882bb74d0ce
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-03-12 17:22:12 +0300

    v4

---
+++ Helper.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task33.task3310;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class Helper {
+
+    public static void printMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String generateRandomString(){
+        SecureRandom random = new SecureRandom();
+        return new BigInteger(130, random).toString(32);
+    }
+
+
+
+
+
+}
